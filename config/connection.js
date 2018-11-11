09205ca49ee3f3625134739e0ad51e3b0aca6e70
LatoyaBogle <41763275+LatoyaBogle@users.noreply.github.com>
2018-11-11 00:54:02 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,20 @@ var sequelize = new Sequelize("burgers_db", "root", "", {
     
   });
   
-
+var connection;
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+    connection= mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection =mysql.createConnection({
     host: 'localhost',
     //port:3000,
     user: 'root',
     password:"",
     database:'burgers_db'
-})
+});
+};
+
 
 connection.connect(function(err){
     if(err)throw err;
