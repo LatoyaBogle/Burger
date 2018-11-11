6d9afb0c11b31731f6e1c029b102761b156369b2
LatoyaBogle <concordiae20@gmail.com>
2018-11-10 23:01:56 -0500

    adding all files for Burger app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Loaded Burger', FALSE);
